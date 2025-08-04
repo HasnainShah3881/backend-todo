e2c2d0468f0f62e3f576d4e3b571239953587831
HasnainShah3881 <hasnainimran3881@gmail.com>
2025-08-04 18:45:21 +0500

    ok

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
- async function connectDB() {
-    await mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.51l7ww2.mongodb.net/test`)
-
+async function connectDB() {
+  await mongoose.connect(
+    `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.51l7ww2.mongodb.net/test`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 }
 
 module.exports = {
-    connectDB
-}
+  connectDB,
+};
