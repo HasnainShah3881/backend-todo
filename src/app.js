08bc7c551f86b92c4550e68af4c57f077cd5cf7a
HasnainShah3881 <hasnainimran3881@gmail.com>
2025-08-11 00:48:30 +0500

    ..

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("CORS not allowed from this origin: " + origin));
