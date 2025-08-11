7248a0d6129ba5f8e4642dd2507490fb549f1b29
HasnainShah3881 <hasnainimran3881@gmail.com>
2025-08-11 14:37:50 +0500

    ok

--- src/app.js
+++ src/app.js
@@ -15,12 +15,12 @@ app.use(express.json());
 
 const allowedOrigins = [
   "https://todo-website-iota.vercel.app",
-  'http://localhost:3000'
+  "http://localhost:3000"
 ];
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (origin || allowedOrigins.includes(origin)) {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("CORS not allowed from this origin: " + origin));
