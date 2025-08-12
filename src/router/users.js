23d79f344e88855d98b41a1a3a5baf46ec71cd90
HasnainShah3881 <hasnainimran3881@gmail.com>
2025-08-12 17:14:21 +0500

    done

--- src/router/users.js
+++ src/router/users.js
@@ -1,23 +1,28 @@
-const express = require("express")
-const { userAuth } = require("../middleware/auth")
-const Usersrouter = express.Router()
+const express = require("express");
+const { userAuth } = require("../middleware/auth");
+const Usersrouter = express.Router();
 
-Usersrouter.get("/getAllUser" , (req , res )=>{
-    res.send("get all users success fully")
-})
+Usersrouter.get("/getAllUser", (req, res) => {
+  res.send("get all users success fully");
+});
 
-Usersrouter.get("/getUser" , userAuth , async(req , res )=>{
-    try {
-        const user = req.user;
-        if(!user){
-        throw new Error("user is not found")
-        }
-        res.status(200).json(user);
-        res.send( user )
-    } catch (error) {
-        res.status(400)
-        console.log(error)
+Usersrouter.get("/getUser", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found." });
     }
-})
+    res.status(200).json({
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: "Error fetching user." });
+    console.log(error);
+  }
+});
 
-module.exports = Usersrouter
+module.exports = Usersrouter;
